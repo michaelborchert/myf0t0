1bc43f70bd615382777403a719b7aee1e5e6613e
Michael Borchert <michael.borchert@gmail.com>
2020-11-25 15:55:51 -0600

    Adding unsaved files.

--- util/processor/deploy.sh
+++ util/processor/deploy.sh
@@ -6,4 +6,3 @@ zip -r9 ${OLDPWD}/function.zip *PIL* *pil* *dateutil* *pytz*
 cd $OLDPWD
 zip -g function.zip photo-processor.py
 aws s3 cp ./function.zip s3://myf0t0dist/photo-processor.zip
-#aws lambda update-function-code --function-name photo-processor --zip-file fileb://function.zip --region us-east-2
