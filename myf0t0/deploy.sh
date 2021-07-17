4f308666333df86a6431d2f24f8e3f99dd801878
Michael Borchert <michael.borchert@gmail.com>
2021-07-16 22:50:44 -0500

    Added build/deploy script for compiled React code, added CloudFront distribution to CFN template, secured CFN-to-S3 connection, updated allowed redirect URL's for Cognito App Client, added prod .env file.

---
+++ myf0t0/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [[ -n $1 ]]; then
+  npx browserslist@latest --update-db
+
+  npm run build
+
+  BUCKET_NAME=$(aws s3api list-buckets --query 'Buckets[*].[Name]' --output text | grep "$1-appbucket")
+  aws s3 sync ./build s3://$BUCKET_NAME
+fi
